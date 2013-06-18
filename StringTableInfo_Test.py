376eb0286b83e6e59d6867d21689dad5605fa30d
Mike Shi <mikeshi80@gmail.com>
2013-06-18 16:28:42 +0800

    fix the unitest to meet the new function declaration.

--- StringTableInfo_Test.py
+++ StringTableInfo_Test.py
@@ -6,7 +6,7 @@ import StringTableInfo
 
 class StringTableInfo_Test(unittest.TestCase):
     def test_NewStringTableInfo(self):
-        sti = StringTableInfo.StringTableInfo('hello', 5, 'yeah hello world')
+        sti = StringTableInfo.StringTableInfo('hello', 5, 10, 'yeah hello world')
         self.assertEqual(sti.string, 'hello')
         self.assertEqual(sti.begin, 5)
         self.assertEqual(sti.end, 10)
